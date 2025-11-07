51dad908a20a6192bb1791be4316ada245cf67e4
Med-Ouallou <mouallou279@gmail.com>
2025-11-07 10:41:27 +0000

    Add blog design with html and tailwinde

--- blog_project/database/seeders/ArticleSeeder.php
+++ blog_project/database/seeders/ArticleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AricleSeeder extends Seeder
+class ArticleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
